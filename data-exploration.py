1bc0dfdd6c62c69c31c6dac30a4af8fd3862d9f8
tim <tim_stehle@posteo.de>
2021-01-02 00:01:36 +0100

    add gitignore and change dataset

--- data-exploration.py
+++ data-exploration.py
@@ -2,9 +2,9 @@ import pandas as pd
 import numpy as np
 import seaborn as sns
 
-# we use the iris dataset from seaborn
-df = sns.load_dataset('iris')
-
+# seaborn comes with build in datasets which we will use
+df = sns.load_dataset('tips')
+print(df)
 # show top and bottom of dataset
 df.head()
 df.tail()
@@ -12,8 +12,13 @@ df.tail()
 # this shows the shape (number of rows and columns)
 df.shape()
 
-# check basic statistics
-df.describe()
-
 # check data types and more
 df.info()
+
+# check basic statistics
+df.describe()  # analyzes numberic numbers only
+# if other data types are there as well you can include it, here with object
+df.describe(include=np.object)
+
+# count number of the items of a specfic column
+df['day'].value_counts()
