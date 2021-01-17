cbe328951ac1cbd170e694ce6a3a4aca27d7e473
tim <tim_stehle@posteo.de>
2021-01-17 23:59:40 +0100

    handle columns

--- data-manipulation.py
+++ data-manipulation.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import numpy as np
 
 # create example dataframe
 df = pd.DataFrame({'num_legs': [2, 4, 8, 0],
@@ -6,10 +7,28 @@ df = pd.DataFrame({'num_legs': [2, 4, 8, 0],
                    'num_specimen_seen': [10, 2, 1, 8]},
                   index=['falcon', 'dog', 'spider', 'fish'])
 
+# -------------------------
+#  handle columns
 
 # rename columns
 df = df.rename(columns={'num_legs': 'renamed_leg'})
 
-# delete columns
+# drop / delete columns
+df = df.drop('num_legs', axis=1)
+del df['num_wings']
 
-print(df)
+# add new column
+color = ['blue', 'red', 'yellow', "green"]
+df['new_column'] = color
+
+# -------------------------
+#  handle missing values
+
+# Convert '?' to NaN
+df[df == '?'] = np.nan
+
+# Print the number of NaNs
+print(df.isnull().sum())
+
+# Drop missing values
+df = df.dropna()
