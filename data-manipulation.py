090dc39478d70ade3bc917451f695d17fb99a6a5
tim <tim_stehle@posteo.de>
2021-01-18 01:27:21 +0100

    pandas groupby

--- data-manipulation.py
+++ data-manipulation.py
@@ -7,6 +7,7 @@ df = pd.DataFrame({'num_legs': [2, 4, 8, 0],
                    'num_specimen_seen': [10, 2, 1, 8]},
                   index=['falcon', 'dog', 'spider', 'fish'])
 
+
 # -------------------------
 #  handle columns
 
@@ -21,6 +22,7 @@ del df['num_wings']
 color = ['blue', 'red', 'yellow', "green"]
 df['new_column'] = color
 
+
 # -------------------------
 #  handle missing values
 
@@ -29,6 +31,18 @@ df[df == '?'] = np.nan
 
 # Print the number of NaNs
 print(df.isnull().sum())
+df.isna().any()
 
 # Drop missing values
 df = df.dropna()
+
+# drop undefined values
+df = df[(df['column_name'] != 'undefined')]
+
+
+# group by column values
+df.groupby(['column_name1', 'column_name2', 'column_name3'])
+
+# group and calculate mean / median
+df.groupby(['column_name1', 'column_name2', 'column_name3']).mean()
+df.groupby(['column_name1', 'column_name2', 'column_name3']).median()
